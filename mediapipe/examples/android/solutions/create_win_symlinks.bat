a4c5eb9ac07fbc733d13c9cde87db2039a3fdec7
TNBL265 <long_tran@mymail.sutd.edu.sg>
2022-02-17 22:12:15 +0800

    refactor: Refactor res dir and untrack properties files

--- mediapipe/examples/android/solutions/create_win_symlinks.bat
+++ mediapipe/examples/android/solutions/create_win_symlinks.bat
@@ -1,23 +1,23 @@
-@rem Remove the current res dir symlinks that are for Linux and macOS and recreate res dir symlinks for Windows.
+@rem Remove the current hands.src.main.res dir symlinks that are for Linux and macOS and recreate hands.src.main.res dir symlinks for Windows.
 @rem This script needs administrator permission. Must run this script as administrator.
 
 @rem for hands example app.
 cd /d %~dp0
 cd hands\src\main
-rm res
-mklink /d res ..\..\..\res
+rm hands.src.main.res
+mklink /d hands.src.main.res ..\..\..\hands.src.main.res
 
 @rem for facemesh example app.
 cd /d %~dp0
 cd facemesh\src\main
-rm res
-mklink /d res ..\..\..\res
+rm hands.src.main.res
+mklink /d hands.src.main.res ..\..\..\hands.src.main.res
 
 @rem for face detection example app.
 cd /d %~dp0
 cd facedetection\src\main
-rm res
-mklink /d res ..\..\..\res
+rm hands.src.main.res
+mklink /d hands.src.main.res ..\..\..\hands.src.main.res
 
 dir
 pause
